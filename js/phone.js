07ee6185a932b659525a75d6c692795a8684b8ad
naimurbd <naiemnaiem072@gmail.com>
2022-03-01 15:07:19 +0600

    main part commit

--- js/phone.js
+++ js/phone.js
@@ -1,27 +1,40 @@
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // console.log(searchText);
+    // clear data 
     searchField.value = '';
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    // console.log(url);
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.data));
-}
+    if (searchText == '') {
+        //  please write something to display 
+    }
+    else {
+        // load data 
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.data));
+    }
+
+
+}
+//  display data 
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    if (data.length == 0) {
+
+    }
     data.forEach(item => {
         console.log(item);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div onclick="loadPhoneDetail('${item.slug}')" class="card h-100">
-                <img src="${item.image}" class="card-img-top" alt="...">
+        <div class="card h-100">
+                <img src="${item.image}" class="card-img-top w-50" alt="...">
                 <div class="card-body">
                     <h3 class="card-title">${item.phone_name}</h3>
                     <h5>${item.brand}</h5>
+                    <button class = "btn btn-info" onclick="loadPhoneDetail('${item.slug}')">Details Explore </button>
                </div>
             </div>
         `;
@@ -43,10 +56,13 @@ const displayPhoneDetail = phone => {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-    <img src="${phone.image}" class="card-img-top" alt="...">
-    <div class="card-body">
+    <img src="${phone.image}" class="card-img-top w-50" alt="...">
+    <div class="card-body ">
         <h5 class="card-title">${phone.name}</h5>
+        <h6> ${phone.releaseDate}</h6>
         <p class="card-text">${phone.mainFeatures.storage}</p>
+        <p class="card-text">${phone.mainFeatures.chipSet}</p>
+        <p class="card-text">${phone.mainFeatures.memory}</p>
         
     </div>
     `;
