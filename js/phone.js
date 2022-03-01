dcd35c1d3fb37bc16f8e15de7ca1b0c40ca1b4a1
naimurbd <naiemnaiem072@gmail.com>
2022-03-01 09:24:38 +0600

    display dynamic search result

--- js/phone.js
+++ js/phone.js
@@ -7,7 +7,24 @@ const searchPhone = () => {
     // console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaySearchResult(data.data));
+}
 
-
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(item => {
+        console.log(item);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+                <img src="${item.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h3 class="card-title">${item.phone_name}</h3>
+                    <h5>${item.brand}</h5>
+               </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
